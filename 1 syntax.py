3e6f7fe77f361e1e3d80f25c32c99694ebcec22b
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-31 23:00:14 -0400

    commit to 1 , 2 and section 3

---
+++ 1 syntax.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+
+a = 1
+if a >= 1:
+  print("a is greater thanor equal to 1")
+
+  
---
+++ 3 variables.py
@@ -0,0 +1,41 @@
+# Create a variable named carname and assign the value Volvo to it.
+
+carname = "volvo"
+
+if carname == "volvo":
+  print("your car is a volvo")
+else:
+   print("your car is not a volvo")
+
+# Create a variable named x and assign the value 50 to it.
+
+x = 50
+
+if x >= 1:
+   print("damage")
+
+
+# Display the sum of 5 + 10, using two variables: x and y.
+
+x = 50
+y = 10
+
+print(x + y(type))
+
+# Create a variable called z, assign x + y to it, and display the result.
+
+x = 10
+y = 5
+z = x + y
+
+print(y)
+
+# Insert the correct syntax to assign the same value to all three variables in one code line.
+
+x = y = z = "Orange"
+
+# Insert the correct keyword to make the variable x belong to the global scope.
+
+
+
+
